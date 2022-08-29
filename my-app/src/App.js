ea98e1d28ad627e4b00a4778f1cf0558432da8b0
veevekdave <veevekd401@gmail.com>
2022-08-28 23:56:22 -0400

    added dace detection box

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,7 +19,24 @@ class App extends Component {
       'imageUrl': ''
     }
   }
+
+   calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+   }
    
+   displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+   }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
@@ -30,14 +47,8 @@ class App extends Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err){
-
-      }
-    );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
   render() {
     return (
@@ -50,7 +61,7 @@ class App extends Component {
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange}
         onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>  
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>  
       </div>
     );
   }
--- my-app/src/components/FaceRecognition/FaceRecognition.js
+++ my-app/src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import "./FaceRecognition.css"
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box}) => {
   console.log("check", imageUrl);
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-      <img alt="" src={imageUrl} width='500px' height='auto' />
+      <img alt="" id="inputimage" src={imageUrl} width='500px' height='auto' />
+      <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
+
+      </div>
       </div>
     </div>
   )
